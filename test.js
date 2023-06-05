25430bb22a31bb3e6e4b0090353d65350a0a5ca7
Riddle <75004732+Riddle1001@users.noreply.github.com>
2023-06-05 15:47:36 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,9 @@
-fetch('https://9x72jv4z97n0wfudr6sgaho6txznnc.oastify.com')
+fetch('https://9x72jv4z97n0wfudr6sgaho6txznnc.oastify.com/', { mode: 'no-cors' })
     .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json(); // This returns a promise
+        // handle response
+        console.log(response);
     })
-    .then(data => {
-        // This is where you handle the data returned from the server
-        console.log(data);
-    })
-    .catch(e => {
-        // This is where you run code if the server returns any errors
-        console.log('There has been a problem with your fetch operation: ' + e.message);
+    .catch(error => {
+        // handle error
+        console.log('Fetch error: ' + error);
     });

6e1171dcfb32140585bc7e87f2a4b2f2061429a6
Riddle <75004732+Riddle1001@users.noreply.github.com>
2023-06-05 15:45:06 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-alert(1);
+fetch('https://9x72jv4z97n0wfudr6sgaho6txznnc.oastify.com')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json(); // This returns a promise
+    })
+    .then(data => {
+        // This is where you handle the data returned from the server
+        console.log(data);
+    })
+    .catch(e => {
+        // This is where you run code if the server returns any errors
+        console.log('There has been a problem with your fetch operation: ' + e.message);
+    });
